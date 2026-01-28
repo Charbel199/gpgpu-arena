fb819b586b4db0111b18c8ee2b138a8de1489a08
Charbel199 <charbel-boumaroun@outlook.com>
2026-01-28 15:45:25 +0100

    Clean up

--- include/arena/kernel_loader.hpp
+++ include/arena/kernel_loader.hpp
@@ -13,14 +13,9 @@ public:
     KernelLoader() = default;
     ~KernelLoader();
 
-    // Load a kernel module from PTX or CUBIN file
-    // Returns a module handle for later reference
     CUmodule load_module(const std::string& path);
-
-    // Get a kernel function from a loaded module
     CUfunction get_function(CUmodule module, const std::string& kernel_name);
 
-    // Launch a kernel with timing
     struct LaunchConfig {
         unsigned int grid_x = 1, grid_y = 1, grid_z = 1;
         unsigned int block_x = 1, block_y = 1, block_z = 1;
@@ -34,7 +29,6 @@ public:
 
     LaunchResult launch(CUfunction func, const LaunchConfig& config, void** args);
 
-    // Unload all modules
     void unload_all();
 
 private:
